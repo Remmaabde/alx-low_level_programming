852dff0a1e6b5faf041a619e55a496384ecef33a
Remmaabde <Reemabdella702@gmail.com>
2022-09-08 14:01:04 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,21 +2,15 @@
 #include <unistd.h>
 
 
-
 /**
- *
- *  * main - Prints to the standard error
- *
- *   * Return: Always 0 (Success)
- *
- *    */
-
+ * main - prints to the standard error
+ * Return : Always 0 (success)
+ */
 int main(void)
-
 {
-
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	
-			return (1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+
+
+
