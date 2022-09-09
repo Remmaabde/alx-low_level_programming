4946eb7206b495514eb8c1a18ad4a9274c2d388f
Remmaabde <Reemabdella702@gmail.com>
2022-09-09 14:25:47 -0700

    100-print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ * Description: print
+ *
+ *
+ *
+ */
+int main()
+{
+	int n1 = 0, n2;
+	while(n <= 9)
+	{
+
+		if (n1 != n2 && n1<n2) 
+		{
+			putchar(n1 + 48);
+			putchar(n2 + 48);
+		       
+			if (n1+n2 !=17)
+			{
+				putchar(',');
+				putchar(' ');
+	                }
+		}
+		++n2;
+
+
+			
+	}++n1;
+	return 0:
+}
