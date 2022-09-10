5ca22df7a898cfddf6704c172b13ddd18c683044
Remmaabde <Reemabdella702@gmail.com>
2022-09-10 02:20:48 -0700

    6-path

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,47 +1,26 @@
 #include <stdio.h>
 
 
-
 /**
+ * main - prints the size of various types
+ * Description: using the main function
  *
- *  * main - prints the size of various types
+ * this program prints "Programming is like building a multilingual puzzle
  *
- *   *
- *
- *    * Description: using the main function
- *
- *     * this program prints "Programming is like building a multilingual puzzle
- *
- *      * Return: 0 
- *
- *       */
+ * Return: 0 
+ */
 
 int main(void)
-
 {
-
-	        char c;
-
-		        int i;
-
-			        long li;
-
-				        long long lli;
-
-					        float f;
-
-
-
-							printf("Size of a char: %d byte(s)\n", sizeof(c));
-
-								printf("Size of an int: %d byte(s)\n", sizeof(i));
-
-									printf("Size of a long int: %d byte(s)\n", sizeof(li));
-
-										printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-
-											printf("Size of a float: %d byte(s)\n", sizeof(f));
-
-												return (0);
-
+      char c;
+      int i;
+      long li;
+      long long lli;
+      float f;
+      printf("Size of a char: %d byte(s)\n", sizeof(c));
+      printf("Size of an int: %d byte(s)\n", sizeof(i));
+      printf("Size of a long int: %d byte(s)\n", sizeof(li));
+      printf("Size of a long long int: %d byte(s)\n", sizeof(lli));	
+      printf("Size of a float: %d byte(s)\n", sizeof(f));
+      return (0);
 }
